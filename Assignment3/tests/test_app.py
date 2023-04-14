ea332ec2e035b78e4c8f007d2c94d583b30de963
dl1364 <94717418+dl1364@users.noreply.github.com>
2023-04-13 20:17:34 -0500

    Update test_app.py

--- Assignment3/tests/test_app.py
+++ Assignment3/tests/test_app.py
@@ -7,9 +7,7 @@ from Assignment3.app import calculate_bmi, calculate_category
 def test_calculate_bmi(feet, inches, weight, bmi):
     assert calculate_bmi(feet, inches, weight) == bmi
 
-@pytest.mark.parametrize("bmi, category", [(0, "Error"), (.1, "Underweight"), (9.5, "Underweight"), 
-    (18.4, "Underweight"), (18.5, "Normal Weight"), (21.5, "Normal Weight"), (24.9, "Normal Weight"), 
-    (25, "Overweight"), (27.5, "Overweight"), (29.9, "Overweight"), (30, "Obese")])
+@pytest.mark.parametrize("bmi, category", [(0, "Error"), (.1, "Underweight"), (9.5, "Underweight"), (18.4, "Underweight"), (18.5, "Normal Weight"), (21.5, "Normal Weight"), (24.9, "Normal Weight"), (25, "Overweight"), (27.5, "Overweight"), (29.9, "Overweight"), (30, "Obese")])
 def test_calculate_category(bmi, category):
     assert calculate_category(bmi) == category
 
@@ -23,8 +21,7 @@ def test_page_status(client):
 
     assert response.status_code == 200
 
-@pytest.mark.parametrize("feet, inches, weight, bmi, category", [(5, 3, 100, 18, "Underweight"), 
-    (5, 3, 125, 22.7, "Normal Weight"), (5, 3, 140, 25, "Overweight"), (5, 3, 180, 32.7, "Obese")])
+@pytest.mark.parametrize("feet, inches, weight, bmi, category", [(5, 3, 100, 18, "Underweight"), (5, 3, 125, 22.7, "Normal Weight"), (5, 3, 140, 25, "Overweight"), (5, 3, 180, 32.7, "Obese")])
 
 def test_user_input(client, feet, inches, weight, bmi, category):
     landing = client.post("/home", method="POST", data = { "feet" : feet, "inches" : inches, "weight" : weight })
